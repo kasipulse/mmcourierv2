bbb47d8d319a45fced87b457815651c9f413648e
kasipulse <148783028+kasipulse@users.noreply.github.com>
2025-10-28 12:58:59 +0200

    Update auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,6 +1,6 @@
 // auth.js
 const SUPABASE_URL = "https://lavqgvnjdjfywcjztame.supabase.co";
-const SUPABASE_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhdnFndm5qZGpmeXdjanp0YW1lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyMjk0ODYsImV4cCI6MjA3NjgwNTQ4Nn0.kpguG-8Ap_icuh1FtF6c4k032qwIvoW6-KC_tX57644";
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 const form = document.getElementById("login-form");
