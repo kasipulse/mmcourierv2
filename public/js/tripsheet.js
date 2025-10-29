022c0e037537468e7e9f8f3c414636b786310d9f
kasipulse <148783028+kasipulse@users.noreply.github.com>
2025-10-29 17:06:16 +0200

    Update tripsheet.js

--- public/js/tripsheet.js
+++ public/js/tripsheet.js
@@ -1,6 +1,6 @@
 // Initialize Supabase
-const SUPABASE_URL = "https://YOUR_PROJECT.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_ANON_KEY";
+const SUPABASE_URL = "https://lavqgvnjdjfywcjztame.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhdnFndm5qZGpmeXdjanp0YW1lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyMjk0ODYsImV4cCI6MjA3NjgwNTQ4Nn0.kpguG-8Ap_icuh1FtF6c4k032qwIvoW6-KC_tX57644";
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 const userEmailEl = document.getElementById("user-email");
