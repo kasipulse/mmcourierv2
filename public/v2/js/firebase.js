aea535d9bb0b009ea7a49e8bcbdf6ff1cd97733b
kasipulse <148783028+kasipulse@users.noreply.github.com>
2026-01-15 17:21:08 +0200

    Update firebase.js

--- public/v2/js/firebase.js
+++ public/v2/js/firebase.js
@@ -1,17 +1,19 @@
+// /js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
+import { getFirestore, serverTimestamp, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDVn4lqyV-I9Z1tQNKnrbHEZCvdNY1FDY4",
   authDomain: "mm-courier-software.firebaseapp.com",
   projectId: "mm-courier-software",
-  storageBucket: "mm-courier-software.appspot.com",
+  storageBucket: "mm-courier-software.firebasestorage.app",
   messagingSenderId: "1088997998974",
-  appId: "1:1088997998974:web:7d31deb10940ef2b4af32f"
+  appId: "1:1088997998974:web:7d31deb10940ef2b4af32f",
+  measurementId: "G-S9MW03EQ5C"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const serverTime = serverTimestamp;
